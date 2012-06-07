5cf651057e9f3a18187fa5bbd39f693e2a3e9d03
Kilian Evang <noreply@texttheater.net>
2012-06-07 14:50:28 +0200

    improved handling of options, added quoting for output, added option to end each output term with a period

--- src/prolog/plep.pl
+++ src/prolog/plep.pl
@@ -22,68 +22,95 @@
 :- use_module(library(optparse),[opt_parse/5]).
 :- use_module(library(charsio),[read_term_from_chars/3]).
 
+:- dynamic option/1.
+
 main :-
   current_prolog_flag(argv,Argv),
   opt_parse([[opt(module),
-                  type(atom),
-                  meta('MODULE'),
-                  help('A module specification to use for operators, in order to be able to parse the term and the files to search in.'),
-                  shortflags([m]),
-                  longflags([module])]],
+              type(atom),
+              meta('MODULE'),
+              help('A module specification to use for operators, in order to be able to parse the term and the files to search in.'),
+              shortflags([m]),
+              longflags([module])],
+             [opt(period),
+              type(boolean),
+              help('If this option is given, each term will be suffixed by a period so the output can be read in as Prolog source.'),
+              shortflags([p]),
+              longflags([period])]],
                 Argv,
                 Options,
                 PositionalArguments),
-  process_options(Options,ReadTermOptionsList),
-  process_positional_arguments(PositionalArguments,ReadTermOptionsList,Term,Files),
-  plep(Files,Term,ReadTermOptionsList),
+  assert_options(Options),
+  process_positional_arguments(PositionalArguments,Term,Files),
+  plep(Files,Term),
   halt.
 main :-
   halt(1).
 
-process_options([],[]).
-process_options([module(Module)|_],[module(Module)]) :-
-  nonvar(Module),
-  !.
-process_options([_|Rest],ReadTermOptionsList) :-
-  process_options(Rest,ReadTermOptionsList).
+assert_options([]).
+assert_options([Option|Options]) :-
+  assert(option(Option)),
+  assert_options(Options).
 
-process_positional_arguments([_,TermAtom|Files],ReadTermOptionsList,Term,Files) :-
+assert_option_if_value_nonvar(Option) :-
+  Option =.. [_|Value],
+  nonvar(Value),
+  !,write('asserting option '), write(Option), nl,
+  assert(option(Option)).
+assert_option_if_value_nonvar(_).
+
+process_positional_arguments([_,TermAtom|Files],Term,Files) :-
   !,
   atom_chars(TermAtom,TermCodes),
-  read_term_from_chars(TermCodes,Term,ReadTermOptionsList).
-process_positional_arguments(_,_,_,_) :-
+  read_term_options_list(RTOL),
+  read_term_from_chars(TermCodes,Term,RTOL).
+process_positional_arguments(_,_,_) :-
   format(user_error,'ERROR: At least one argument is required: TERM [FILE...]~n',[]),
   fail.
 
-plep([],_,_).
-plep([File|Files],Term,RTOL) :-
-  plep_file(File,Term,RTOL),
-  plep(Files,Term,RTOL).
+plep([],_).
+plep([File|Files],Term) :-
+  plep_file(File,Term),
+  plep(Files,Term).
 
-plep_file(File,Term,RTOL) :-
+plep_file(File,Term) :-
   open(File,read,Stream), % TODO handle file non-existence, maybe by trying to suffix filename with .pl?
-  plep_stream(Stream,Term,RTOL),
+  plep_stream(Stream,Term),
   close(Stream).
 
-plep_stream(Stream,SearchTerm,RTOL) :-
+plep_stream(Stream,SearchTerm) :-
+  read_term_options_list(RTOL),
   read_term(Stream,ReadTerm,RTOL),
   plep_term(ReadTerm,SearchTerm),
-  plep_stream_continue(ReadTerm,Stream,SearchTerm,RTOL).
+  plep_stream_continue(ReadTerm,Stream,SearchTerm).
 
-plep_stream_continue(end_of_file,_,_,_) :-
+read_term_options_list([module(Module)]) :-
+  option(module(Module)),
+  nonvar(Module),
   !.
-plep_stream_continue(_,Stream,SearchTerm,RTOL) :-
-  plep_stream(Stream,SearchTerm,RTOL).
+read_term_options_list([]).
+
+plep_stream_continue(end_of_file,_,_) :-
+  !.
+plep_stream_continue(_,Stream,SearchTerm) :-
+  plep_stream(Stream,SearchTerm).
 
 % If the read term can unify with the search term, print it, otherwise recurse.
 plep_term(ReadTerm,SearchTerm) :-
   \+ \+ (ReadTerm = SearchTerm),
   !,
-  write(ReadTerm), % TODO prefix with filename if requested
+  write_term(ReadTerm,[quoted(true)]), % TODO prefix with filename if requested
+  write_period_if_requested,
   nl.
 plep_term(ReadTerm,SearchTerm) :-
   ReadTerm =.. [_|Terms],
   plep_terms(Terms,SearchTerm).
+
+write_period_if_requested :-
+  option(period(true)),
+  !,
+  write('.').
+write_period_if_requested.
 
 plep_terms([],_).
 plep_terms([Term|Terms],SearchTerm) :-
